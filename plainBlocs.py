ca532806f8da4e0bdd8fb499795d81337406172e
loris <loris.obry@gmail.com>
2025-01-31 09:17:37 +0100

    get textures from blocs names

--- plainBlocs.py
+++ plainBlocs.py
@@ -1,13 +1,19 @@
 import minecraft_data
 
-mcd = minecraft_data("1.19.2")
-plain_blocs = []
-for b in mcd.blocks_list:
-    if (b['boundingBox'] == 'block' and mcd.blockCollisionShapes['blocks'][b['name']] == 1 ):
-        plain_blocs.append((b['id'], b['name']))
+def getBlocs():
+    mcd = minecraft_data("1.19.2")
+    plain_blocs = []
+    for b in mcd.blocks_list:
+        if (b['boundingBox'] == 'block' and mcd.blockCollisionShapes['blocks'][b['name']] == 1 ):
+        #    plain_blocs.append((b['id'], b['name'])) # ID and Name
+            plain_blocs.append(b['name']) # name only
+    return plain_blocs;
         
-print(plain_blocs)
-print(len(plain_blocs))
+
+if __name__ == "__main__":
+    print(getBlocs())
+
+
         
 #print(mcd.blockCollisionShapes['blocks']['cobblestone'])
 #Degager les blocs transparants
